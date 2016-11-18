db9fe00e513525fac4ae5b47be272944296b767b
Jared Heath <jared@piqo.co>
2016-11-18 11:28:08 -0500

    [Fix] Use correct field names in SurveySchema

--- server/models/schemas/SurveySchema.js
+++ server/models/schemas/SurveySchema.js
@@ -4,8 +4,9 @@ var ObjectId = mongoose.Schema.Types.ObjectId;
 var SurveySchema = new mongoose.Schema({
   title: {type: String, required: true},
   questionCount: {type: Number, required: true},
-  durationMinutes: {type: Number, required: true},
-  rewardCredits: {type: Number, required: true}
+  estimatedMinutes: {type: Number, required: true},
+  credits: {type: Number, required: true},
+  responses: [{type: ObjectId, ref: 'response', required: true}]
 }, {timestamps: true});
 
 module.exports = SurveySchema;
